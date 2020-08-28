3db91ed330a1abfc48c160ab9a97d39a444db464
Elbert Ballo <eballofullstackdev@gmail.com>
2020-08-28 00:58:49 -0700

    Squashing the commits
    commit b7960eaa417c56addbffdc9316a76b390d05f1d1
    Author: Elbert Ballo <eballofullstackdev@gmail.com>
    Date:   Fri Aug 28 00:50:53 2020 -0700
    
        this is the second update
    
    commit 7f08383d99bfa978bfde01a1c0bf92b73907f779
    Author: Elbert Ballo <eballofullstackdev@gmail.com>
    Date:   Fri Aug 28 00:48:12 2020 -0700
    
        this is the next commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Hello this is the first change</h1>
+          <h1>This is the second change</h1>
         </p>
         <a
           className="App-link"
